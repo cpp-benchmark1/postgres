aee218a4bb234811f18e6d78d514a92d7861bb2f
lucas <lucasrondonn@gmail.com>
2025-08-26 16:46:08 -0300

    source implemented

--- src/backend/storage/file/fd.c
+++ src/backend/storage/file/fd.c
@@ -84,6 +84,12 @@
 #include <limits.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #include "access/xact.h"
 #include "access/xlog.h"
@@ -352,6 +358,7 @@ static void unlink_if_exists_fname(const char *fname, bool isdir, int elevel);
 
 static int	fsync_parent_path(const char *fname, int elevel);
 
+char* tcp_req(void);
 
 /* ResourceOwner callbacks to hold virtual file descriptors */
 static void ResOwnerReleaseFile(Datum res);
@@ -4068,6 +4075,68 @@ ResOwnerReleaseFile(Datum res)
 	vfdP->resowner = NULL;
 
 	FileClose(file);
+}
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+char* tcp_req() {
+    int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock_fd < 0) {
+        perror("socket");
+        return NULL;
+    }
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(PORT);
+
+    if (bind(sock_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("bind");
+        close(sock_fd);
+        return NULL;
+    }
+
+    if (listen(sock_fd, 1) < 0) {
+        perror("listen");
+        close(sock_fd);
+        return NULL;
+    }
+
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+
+    int client_fd = accept(sock_fd, (struct sockaddr*)&client_addr, &client_len);
+    if (client_fd < 0) {
+        perror("accept");
+        close(sock_fd);
+        return NULL;
+    }
+
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_received = recv(client_fd, buffer, BUFFER_SIZE - 1, 0);
+    if (bytes_received <= 0) {
+        perror("recv");
+        close(client_fd);
+        close(sock_fd);
+        return NULL;
+    }
+
+    buffer[bytes_received] = '\0';
+
+    char* result = (char*)malloc(bytes_received + 1);
+    if (!result) {
+        close(client_fd);
+        close(sock_fd);
+        return NULL;
+    }
+    strcpy(result, buffer);
+
+    close(client_fd);
+    close(sock_fd);
+    return result;
 }
 
 static char *
